179c6bb17e01f1bbd78d34bd4a342a0ca7e86d58
kankan-web <2728360489@qq.com>
2024-05-05 17:34:16 +0800

    NPM:引入tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import preact from "@astrojs/preact";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), preact()]
+  integrations: [mdx(), sitemap(), preact(), tailwind()]
 });
