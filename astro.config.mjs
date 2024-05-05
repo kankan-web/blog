ec654f274dd7151d0bbe455cb8015d9606ac5aaa
kankan-web <2728360489@qq.com>
2024-05-05 21:42:05 +0800

    NPM:安装astro-icon和ts-plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,23 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), preact(), tailwind(), icon()]
+  site: "https://example.com",
+  base: "/",
+  integrations: [
+    mdx(),
+    sitemap(),
+    preact(),
+    tailwind(),
+    icon({
+      include: {
+        "material-symbols": ["*"],
+      },
+    }),
+  ],
 });
