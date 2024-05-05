f3000fb0b821967636cd25257437abbbc50e94d2
kankan-web <2728360489@qq.com>
2024-05-05 17:35:44 +0800

    NPM:引入astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import preact from "@astrojs/preact";
-
 import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), preact(), tailwind()]
+  integrations: [mdx(), sitemap(), preact(), tailwind(), icon()]
 });
