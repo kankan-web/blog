d31d9858cfc29f7621dbfff78aca67cff2025fd3
kankan-web <2728360489@qq.com>
2024-09-07 23:19:17 +0800

    chore:引入reading-time用于计算阅读分数与mdast-util-to-string提取Markdown中的所有文本

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,15 @@ import sitemap from "@astrojs/sitemap";
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
+import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   base: "/",
   integrations: [mdx(), sitemap(), preact(), tailwind(), icon()],
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
 });
 
