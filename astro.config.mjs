ef9847a5f15464fad788d5961c56916b8285a789
kankan-web <2728360489@qq.com>
2024-12-08 11:53:39 +0800

    feat:移除无用的代码

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,25 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
-import icon from 'astro-icon';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
+import icon from 'astro-icon'
 //关于markdown相关的配置
-import remarkMath from 'remark-math'; //数学表达式
-import remarkDirective from 'remark-directive'; //自定义指令
-import { remarkEmbed } from './src/plugins/remarkEmbed'; //允许插入外部资源
-import { remarkSpoiler } from './src/plugins/remarkSpoiler'; //允许创建可折叠信息
-import { remarkReadingTime } from './src/plugins/remarkReadingTime';
+import remarkMath from 'remark-math' //数学表达式
+import remarkDirective from 'remark-directive' //自定义指令
+import { remarkEmbed } from './src/plugins/remarkEmbed' //允许插入外部资源
+import { remarkSpoiler } from './src/plugins/remarkSpoiler' //允许创建可折叠信息
+import { remarkReadingTime } from './src/plugins/remarkReadingTime'
 //关于markdown渲染相关配置
-import { rehypeHeadingIds } from '@astrojs/markdown-remark'; //为每个标题新建一个id
-import rehypeKatex from 'rehype-katex'; //渲染数学公式
-import { rehypeLink } from './src/plugins/rehypeLink'; //链接增强
-import { rehypeImage } from './src/plugins/rehypeImage'; //图片增强
-import { rehypeHeading } from './src/plugins/rehypeHeading'; //标题头部增强
-import { rehypeCodeBlock } from './src/plugins/rehypeCodeBlock'; //代码渲染优化
-import { rehypeCodeHighlight } from './src/plugins/rehypeCodeHighlight'; //语法高亮
+import { rehypeHeadingIds } from '@astrojs/markdown-remark' //为每个标题新建一个id
+import rehypeKatex from 'rehype-katex' //渲染数学公式
+import { rehypeLink } from './src/plugins/rehypeLink' //链接增强
+import { rehypeImage } from './src/plugins/rehypeImage' //图片增强
+import { rehypeHeading } from './src/plugins/rehypeHeading' //标题头部增强
+import { rehypeCodeBlock } from './src/plugins/rehypeCodeBlock' //代码渲染优化
+import { rehypeCodeHighlight } from './src/plugins/rehypeCodeHighlight' //语法高亮
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless' // 确保使用正确的适配器
 
 // https://astro.build/config
 export default defineConfig({
@@ -35,8 +35,17 @@ export default defineConfig({
     remarkPlugins: [remarkMath, remarkDirective, remarkEmbed, remarkSpoiler, remarkReadingTime],
     //定制对你的 Markdown 输出内容的处理方式
     //https://docs.astro.build/zh-cn/reference/configuration-reference/#markdownrehypeplugins
-    rehypePlugins: [rehypeHeadingIds, rehypeKatex, rehypeLink, rehypeImage, rehypeHeading, rehypeCodeBlock, rehypeCodeHighlight]
+    rehypePlugins: [
+      rehypeHeadingIds,
+      rehypeKatex,
+      rehypeLink,
+      rehypeImage,
+      rehypeHeading,
+      rehypeCodeBlock,
+      rehypeCodeHighlight,
+    ],
   },
-  output: "server",
-  adapter: vercel()
-});
+  outDir: 'dist', // 确保输出目录设置正确
+  output: 'server',
+  adapter: vercel(),
+})
